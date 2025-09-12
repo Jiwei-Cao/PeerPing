c76ca984bdfc47f8fb7fb3ea3545cf80efdef7fd
Jiwei Cao <jiweicao2006@gmail.com>
2025-09-12 17:13:08 +0100

    app file

---
+++ app/src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Toast from 'react-native-toast-message';
+import { NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { RootNavigator } from '@navigation/RootNavigator';
+import { AuthProvider } from '@features/auth/AuthProvider';
+import { ThemeProvider } from '@theme/ThemeProvider';
+import { toastConfig } from '@utils/toastConfig';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2,
+      refetchOnWindowFocus: false,
+      staleTime: 5 * 60 * 1000, // 5 minutes
+    },
+  },
+});
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <AuthProvider>
+            <NavigationContainer>
+              <StatusBar style="auto" />
+              <RootNavigator />
+              <Toast config={toastConfig} />
+            </NavigationContainer>
+          </AuthProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </GestureHandlerRootView>
+  );
+}
