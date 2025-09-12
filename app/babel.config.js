626a31863267fd1581fc501c8d31bd0dd0c4fe30
Jiwei Cao <jiweicao2006@gmail.com>
2025-09-12 17:12:48 +0100

    added config files

---
+++ app/babel.config.js
@@ -0,0 +1,26 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+            '@api': './src/api',
+            '@features': './src/features',
+            '@components': './src/components',
+            '@hooks': './src/hooks',
+            '@navigation': './src/navigation',
+            '@utils': './src/utils',
+            '@constants': './src/constants',
+            '@stores': './src/stores'
+          }
+        }
+      ],
+      'react-native-reanimated/plugin'
+    ]
+  };
+};
