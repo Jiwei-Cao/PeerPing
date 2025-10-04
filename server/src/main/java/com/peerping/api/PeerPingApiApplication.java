67392936f434f947785e10bfc7edd97cde006200
Jiwei Cao <jiweicao2006@gmail.com>
2025-10-04 18:20:22 +0100

    created the peerping entry point

---
+++ server/src/main/java/com/peerping/api/PeerPingApiApplication.java
@@ -0,0 +1,13 @@
+package com.peerping.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerPingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeerPingApiApplication.class, args);
+    }
+
+}
