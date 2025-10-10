7890200ae7fe2f4c1f87d4f78642075647e9d56a
Jiwei Cao <jiweicao2006@gmail.com>
2025-10-10 20:58:18 +0100

    fixed user tag

--- server/src/main/java/com/peerping/api/repository/UserTagRepository.java
+++ server/src/main/java/com/peerping/api/repository/UserTagRepository.java
@@ -1,25 +1,23 @@
 package com.peerping.api.repository;
 
-import java.util.List;
-import java.util.UUID;
-
+import com.peerping.api.entity.UserTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.peerping.api.entity.UserTag;
+import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface UserTagRepository extends JpaRepository<UserTag, UserTag.UserTagId>{
+public interface UserTagRepository extends JpaRepository<UserTag, UserTag.UserTagId> {
     List<UserTag> findByUserId(UUID userId);
-
+    
     @Modifying
     @Query("DELETE FROM UserTag ut WHERE ut.user.id = :userId")
-    void deleteAllBYUserId(@Param("userId") UUID userId);
-
+    void deleteAllByUserId(@Param("userId") UUID userId);
+    
     @Query("SELECT ut FROM UserTag ut WHERE ut.user.id = :userId AND ut.intent = :intent")
     List<UserTag> findByUserIdAndIntent(@Param("userId") UUID userId, @Param("intent") UserTag.TagIntent intent);
-
 }
