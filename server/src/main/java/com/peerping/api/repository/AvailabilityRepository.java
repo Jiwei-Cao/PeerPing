f08a0a0990ebd40fe08829e0c009e18fbe352b67
Jiwei Cao <jiweicao2006@gmail.com>
2025-10-11 09:23:42 +0100

    added availability repository

--- server/src/main/java/com/peerping/api/repository/AvailabilityRepository.java
+++ server/src/main/java/com/peerping/api/repository/AvailabilityRepository.java
@@ -1,20 +1,21 @@
 package com.peerping.api.repository;
 
 import com.peerping.api.entity.Availability;
+
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.UUID;
-
 @Repository
-public interface AvailabilityRepository extends JpaRepository<Availability, Availability.AvailabilityId> {
+public interface AvailabilityRepository extends JpaRepository<Availability, Availability.AvailabilityId>{
     List<Availability> findByUserId(UUID userId);
-    
+
     @Modifying
     @Query("DELETE FROM Availability a WHERE a.user.id = :userId")
-    void deleteAllByUserId(@Param("userId") UUID userId);
+    void deleteUserById(@Param("userId") UUID userId);
 }
